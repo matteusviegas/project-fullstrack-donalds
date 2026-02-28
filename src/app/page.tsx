40d06db3beb4e839c6bf8c53dc05352a5d900671
matteus viegas <msmatteusbr2@gmail.com>
2026-02-28 18:02:08 -0300

    chore: add eslint sort pluggin  and prettier tailwind plugin ]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return ( <div className="p-5 border border-red-500 rounded-xl">
+  return ( <div className="p-5  border-red-500 rounded-xl">
   <h1 className="text-red-500">Hello word </h1>
   <button>FSW 2.0</button>
   </div> );
